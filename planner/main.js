da407428bf69dfb3efe1f41c4d253fbff185787c
Mustafa Ismail <mustafais481@gmail.com>
2024-05-22 11:02:40 +0200

    Exercice 3: Spécialisation des workers

--- planner/main.js
+++ planner/main.js
@@ -10,17 +10,16 @@ const taskType = () => (randInt(0, 2) ? 'mult' : 'add')
 const args = () => ({ a: randInt(0, 40), b: randInt(0, 40) })
 
 const generateTasks = (i) =>
-  new Array(i).fill(1).map((_) => ({ type: taskType(), args: args() }))
+  new Array(i).fill(1).map(() => ({ type: taskType(), args: args() }))
 
-let workers = ['http://localhost:8080']
+let workers = {
+  add: [],
+  mult: []
+};
 
 const app = express()
 app.use(express.json())
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-)
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.send(JSON.stringify(workers))
@@ -28,60 +27,67 @@ app.get('/', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { url, id } = req.body
-  console.log(`Register: adding ${url} worker: ${id}`)
-  workers.push({ url, id })
+
+  if (url.includes('worker-add-service')) {
+    workers.add.push({ url, id });
+    console.log(`Register worker-add : adding ${url} worker: ${id}`)
+  } else if (url.includes('worker-mult-service')) {
+    workers.mult.push({ url, id });
+    console.log(`Register worker-mult : adding ${url} worker: ${id}`)
+  }
   res.send('ok')
 })
 
 let tasks = generateTasks(nbTasks)
 let taskToDo = nbTasks
 
-const wait = (mili) =>
-  new Promise((resolve, reject) => setTimeout(resolve, mili))
+const wait = (mili) => new Promise((resolve) => setTimeout(resolve, mili))
 
 const sendTask = async (worker, task) => {
   console.log(`=> ${worker.url}/${task.type}`, task)
-  workers = workers.filter((w) => w.id !== worker.id)
+  workers[task.type] = workers[task.type].filter((w) => w.id !== worker.id)
   tasks = tasks.filter((t) => t !== task)
-  const request = fetch(`${worker.url}/${task.type}`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(task.args),
-  })
-    .then((res) => {
-      workers = [...workers, worker]
-      return res.json()
+  try {
+    const response = await fetch(`${worker.url}/${task.type}`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(task.args)
     })
-    .then((res) => {
-      taskToDo -= 1
-      console.log('---')
-      console.log(nbTasks - taskToDo, '/', nbTasks, ':')
-      console.log(task, 'has res', res)
-      console.log('---')
-      return res
-    })
-    .catch((err) => {
-      console.error(task, ' failed', err.message)
-      tasks = [...tasks, task]
-    })
+    const result = await response.json()
+    workers[task.type].push(worker)
+    taskToDo -= 1
+    console.log('---')
+    console.log(nbTasks - taskToDo, '/', nbTasks, ':')
+    console.log(task, 'has res', result)
+    console.log('---')
+    return result
+  } catch (err) {
+    console.error(task, 'failed', err.message)
+    tasks.push(task)
+    workers[task.type].push(worker)
+  }
 }
 
 const main = async () => {
   console.log(tasks)
   while (taskToDo > 0) {
     await wait(100)
-    if (workers.length === 0 || tasks.length === 0) continue
-    sendTask(workers[0], tasks[0])
+    if (tasks.length === 0) continue
+
+    for (const task of tasks) {
+      if (workers[task.type].length === 0) continue
+      await sendTask(workers[task.type][0], task)
+    }
   }
   console.log('end of tasks')
   server.close()
 }
 
 const server = app.listen(port, () => {
-  console.log(`Register listening at http://localhost:${port}`)
+  console.log(`Planner listening at http://localhost:${port}`)
   console.log('starting tasks...')
   main()
 })
